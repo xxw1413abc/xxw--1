7d2ed1dfb3f88545304134b32cbfc6bb7fc0d1a6
xxw1413abc <1003840365@qq.com>
2025-06-13 18:03:35 +0800

    '1'

--- js/app1.js
+++ js/app1.js
@@ -1,3 +1,4 @@
+//使用const声明变量
 const taskInput = document.getElementById('taskInput');
 const addBtn = document.getElementById('addBtn');
 const taskList = document.getElementById('taskList');
@@ -6,12 +7,12 @@ const sortByNameBtn = document.getElementById('sortByName');
 const sortByDateBtn = document.getElementById('sortByDate');
 let tasks = [
     {
-        text:"ffff4", createdAt: new Date(2025, 2, 14),
+        text:"fff4",createdAt: new Date(2025, 2, 14),
     },{
-        
-        text:"7767764", createdAt: new Date(2025, 1, 16),
+        text:"7767764",createdAt: new Date(2025, 1, 16),
     }
-];
+]
+ 
 function update() {
     taskList.innerHTML = '';
     tasks.forEach(task => {
@@ -19,43 +20,48 @@ function update() {
         const span = document.createElement('span');
         span.className = 'task-text';
         span.textContent = task.text;
+
         const dateSpan = document.createElement('span');
         dateSpan.className = 'task-date';
-        dateSpan.textContent = "("+ task.createdAt.toLocaleDateString() + ")";
+        dateSpan.textContent = "(" + task.createdAt.toLocaleDateString() +")";
         li.appendChild(span);
         li.appendChild(dateSpan);
         taskList.appendChild(li);
     })
-    statusElement.innerHTML = `当前总任务数： ${tasks.length} `;
+    statusElement.innerHTML = `当前总任务数:  {tasks.length}`;
 }
+
 function addTask() {
-   if(!taskInput.value) {
-    alert('Please enter a task');
-    return;
-   }
-   const newtask = {
-    text: taskInput.value,
-    createdAt: new Date(),
-   };
-   tasks.push(newtask);
-   taskInput.value = '';
-   update()
-}
-addBtn.addEventListener('click', addTask);
-taskInput.addEventListener('keypress', (event) => {
+    if(!taskInput.value) {
+     alert('Please enter a task');
+     return;
+    }
+    const newtask = {
+     text: taskInput.value,
+     createdAt: new Date(),
+    };
+    tasks.push(newtask);
+    taskInput.value = '';
+    update()
+ }
+ addBtn.addEventListener('click', addTask);
+ taskInput.addEventListener('keypress', (event) => {
     if(event.key === 'Enter') {
         addTask();
     }
-})
-function sortTasksByName() {
-    tasks.sort((a, b) => a.text.localeCompare(b.text));
+ })
+
+ function sortTasksByName() {
+    tasks.sort((a, b) => a. text.localeCompare(b.text));
     update();
-}
-function sortTasksByDate() {
+ }
+
+ function sortTasksByDate() {
     tasks.sort((a, b) => a.createdAt - b.createdAt);
     update();
-}
+ }
 
-sortByNameBtn.addEventListener('click', sortTasksByName);
-sortByDateBtn.addEventListener('click', sortTasksByDate);
-update();
+ sortByNameBtn.addEventListener('click', sortTasksByName);
+ sortByDateBtn.addEventListener('click', sortTasksByDate);
+ update();
+ 
